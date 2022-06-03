7f604d228360ead9b8110abb619d8142e05e5682
Zerocomes <sergio.aqs17@gmail.com>
2022-06-03 05:56:55 -0500

    Integration Testing (GETs)

---
+++ SmartWorkout-Backend.Tests/Dumbbell/DumbbellControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using SmartWorkout_Backend.Communication;
+using SmartWorkout_Backend.Resources;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartWorkout_Backend.Tests.Dumbbell
+{
+    public class DumbbellControllerTests : IntegrationTest
+    { 
+        [Fact]
+        public async Task GetAllDumbbells_ReturnNotNull()
+        {
+            await AunthenticateAsync();
+
+            var response = await TestClient.GetAsync(ApiRoutes.Dumbbell.GetAllDumbbell);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            (await JsonSerializer.DeserializeAsync<ApiResponse<List<DumbbellResource>>>(await response.Content.ReadAsStreamAsync()))
+                .Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetDumbbellById_ReturnNotNull()
+        {
+            await AunthenticateAsync();
+
+            var response = await TestClient.GetAsync(ApiRoutes.Dumbbell.GetDumbbellById);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            (await JsonSerializer.DeserializeAsync<ApiResponse<DumbbellResource>>(await response.Content.ReadAsStreamAsync()))
+                .Should().NotBeNull();
+        }
+    }
+}
