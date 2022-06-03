f1431ac391b5b9afde8a57bfdc40ae292f21dd69
Zerocomes <sergio.aqs17@gmail.com>
2022-06-03 06:17:39 -0500

    Revert "Revert "Integration Testing (GETs)""
    This reverts commit fa73799a0c51376639662691b41772e540fbe55d.

---
+++ SmartWorkout-Backend.Tests/ExerciseMonitor/ExerciseMonitorControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using SmartWorkout_Backend.Communication;
+using SmartWorkout_Backend.Resources;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartWorkout_Backend.Tests.ExerciseMonitor
+{
+    public class ExerciseMonitorControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAllExerciseMonitorsByUserId_ReturnNotNull()
+        {
+            await AunthenticateAsync();
+
+            var response = await TestClient.GetAsync(ApiRoutes.ExerciseMonitor.GetExerciseMonitorId);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            (await JsonSerializer.DeserializeAsync<ApiResponse<List<ExerciseMonitorResource>>>(await response.Content.ReadAsStreamAsync()))
+                .Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetExerciseMonitorById_ReturnNotNull()
+        {
+            await AunthenticateAsync();
+
+            var response = await TestClient.GetAsync(ApiRoutes.ExerciseMonitor.GetExerciseMonitorId);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            (await JsonSerializer.DeserializeAsync<ApiResponse<ExerciseMonitorResource>>(await response.Content.ReadAsStreamAsync()))
+                .Should().NotBeNull();
+        }
+    }
+}
